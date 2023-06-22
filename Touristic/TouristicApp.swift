8ab572e94c554dedd1c80575e3051aa1a4536f76
InLurker <82582556+InLurker@users.noreply.github.com>
2023-06-22 16:40:23 +0700

    init: Initialized Touristic App Xcode Project

---
+++ Touristic/TouristicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouristicApp.swift
+//  Touristic
+//
+//  Created by Jevin Laudo on 2023-06-22.
+//
+
+import SwiftUI
+
+@main
+struct TouristicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
